4a35216e5696e0a1850f9ad56b021aa66cc1cf0a
omid zarinmahd <zarinpy@gmail.com>
2023-09-05 06:00:55 -0500

    team table name

--- modules/hrm/models/team.py
+++ modules/hrm/models/team.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from modules.common.models import UserModelMixin
+from modules.common.models import UserModelMixin, OrganizationModelMixin, TimestampedModelMixin, ActivatedModelMixin
 
 
-class Team(models.Model):
+class Team(OrganizationModelMixin, TimestampedModelMixin, ActivatedModelMixin):
     name = models.CharField(max_length=300)
+    palace = models.ForeignKey("Palace", on_delete=models.CASCADE, related_name="%(class)ss")
 
     class Meta:
+        db_table = "team"
         verbose_name = _("team")
         verbose_name_plural = _("teams")
 
@@ -16,6 +18,7 @@ class TeamMembers(UserModelMixin):
     team = models.ForeignKey("hrm.Team", on_delete=models.CASCADE, related_name="%(class)ss")
 
     class Meta:
+        db_table = "team_member"
         verbose_name = _("team member")
         verbose_name_plural = _("team members")
 
